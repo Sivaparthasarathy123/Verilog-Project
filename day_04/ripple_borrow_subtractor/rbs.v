a1841626392a5ab0357dbc4d81455b32e8d5537f
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-14 12:45:53 +0530

    Update rbs.v

--- day_04/ripple_borrow_subtractor/rbs.v
+++ day_04/ripple_borrow_subtractor/rbs.v
@@ -2,24 +2,27 @@ module ripple_borrow_subtractor(
       input a,
       input b,
       input bin,     //bin represents borrow input
-      output reg bw,     //bw represents borrow
-      output reg bout);  //bout represents borrow output
- always@(*)begin
-   assign bw = a^b^bin;
-   assign bout = (~a^b)|(~(a^b)&bin);
- end
+      output d,     //d represents difference
+      output bout);  //bout represents borrow output
+   assign d = a^b^bin;
+   assign bout = (a&b)|(b&bin)|(a&bin);
 endmodule
-module ripple_borrow_subtractor(
-      input [3:0]A,
-      input [3:0]B,
-      input Bin,     //Bin represents borrow input
-      output reg Bw,     //Bw represents borrow
-      output reg Bout);
+
+module ripple_borrow_subtractor_4bit(
+      input [3:0]a,
+      input [3:0]b,
+      input bin,     //Bin represents borrow input
+      output [3:0]d,     //D represents Difference
+      output bout);  //Bout represents borrow out-ut
              wire bw1,bw2,bw3;
-      ripple_borrow_subtractor inst1(A[0], B[0], Bin, Bw[0], bw1);
-      ripple_borrow_subtractor inst2(A[1], B[1], bw1, Bw[1], bw2);
-      ripple_borrow_subtractor inst3(A[0], B[0], bw2, Bw[2], bw3);
-      ripple_borrow_subtractor inst4(A[0], B[0], bw3, Bw[3], Bout);
+             wire [3:0]bi=b^{4{bin}};
+             wire binv;
+      ripple_borrow_subtractor inst1(a[0], bi[0], bin, d[0], bw1);
+      ripple_borrow_subtractor inst2(a[1], bi[1], bw1, d[1], bw2);
+      ripple_borrow_subtractor inst3(a[2], bi[2], bw2, d[2], bw3);
+      ripple_borrow_subtractor inst4(a[3], bi[3], bw3, d[3], binv);
+
+      assign bout=~binv;
 endmodule
    
 
