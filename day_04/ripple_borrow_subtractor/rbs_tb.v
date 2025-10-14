c41cd0942cab5b21bf816199295af4874499fc4a
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-14 12:44:07 +0530

    Update rbs_tb.v

--- day_04/ripple_borrow_subtractor/rbs_tb.v
+++ day_04/ripple_borrow_subtractor/rbs_tb.v
@@ -1,21 +1,21 @@
 module ripple_borrow_subtractor_tb();
-  reg A;
-  reg B;
-  reg Bin
-  wire Bw;
-  wire Bout;
+  reg [3:0]a;
+  reg [3:0]b;
+  reg bin;
+  wire [3:0]d;
+  wire bout;
   integer i,j;
-  ripple_borrow_subtractor inst(A,B,Bin,Bw,Bout);
+  ripple_borrow_subtractor_4bit inst(.a(a),.b(b),.bin(bin),.d(d),.bout(bout));
   initial begin
-    $monitor("time=%0t|A=%b|B=%b|Bin=%b|Bw=%b|Bout",$time,A,B,Bin,Bw,Bout);
+    $monitor("time=%0t|A=%b|B=%b|Bin=%b|D=%b|Bout",$time,a,b,bin,d,bout);
     $dumpfile("RBS.vcd");
     $dumpvars;
-    for(i=0;i<8;i++)begin
-      for(j=0;j<8;j++)begin
-        A=i;B=j;Bin=0;
+    for(i=0;i<16;i++)begin
+      for(j=0;j<16;j++)begin
+        a=i;b=j;bin=1;
         #5;
       end
     end
+    $finish;
   end
 endmodule
-    
