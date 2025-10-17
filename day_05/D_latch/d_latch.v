c23b4fff571c85d8216e0a7e31742d7cc3b16fba
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-18 01:57:09 +0530

    d_latch.v

--- day_05/D_latch/d_latch.v
+++ day_05/D_latch/d_latch.v
@@ -7,15 +7,15 @@ module d_latch(
     if(E==1)begin
       case(D)
         1'b0:begin
-          Q=0;Qbar=1;
+          Q<=0;Qbar<=1;
         end
         1'b1:begin
-          Q=1;Qbat=0;
+          Q<=1;Qbar<=0;
         end
       endcase
     end
     else (E==0)begin
-      Q=Q;Qbar=Qbar;
+      Q<=Q;Qbar<=Qbar;
     end
   end
 endmodule
