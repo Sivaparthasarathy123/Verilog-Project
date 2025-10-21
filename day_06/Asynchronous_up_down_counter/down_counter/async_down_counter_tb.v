e27b21c26f178473701d28e3a0cd73245e4c4c58
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-21 23:51:57 +0530

    async_down_counter_tb.v

---
+++ day_06/Asynchronous_up_down_counter/down_counter/async_down_counter_tb.v
@@ -0,0 +1,29 @@
+module async_down_counter_tb();
+    reg clk;
+    reg rst;
+    wire [3:0]q;
+
+  async_down_counter inst(clk,rst,q);
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+   initial begin
+     $monitor("%0t|clk=%b|rst=%b|down=%b",$time,clk,rst,q);
+     $dumpfile("async_up.vcd");
+     $dumpvars(0,async_down_counter_tb);
+    end
+    initial begin
+        rst = 1;
+        #10;           
+        rst = 0;      
+        #100;
+        
+        rst = 1;
+        #10;
+        rst = 0;
+        #100;
+        $finish;
+    end
+endmodule
