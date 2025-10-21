317889e421b8bc8c0521e3718bc9b2673a521fb0
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-21 23:55:50 +0530

    async_up_counter_tb.v

--- day_06/Asynchronous_up_down_counter/up_counter/async_up_counter_tb.v
+++ day_06/Asynchronous_up_down_counter/up_counter/async_up_counter_tb.v
@@ -1,16 +1,16 @@
 module async_up_counter_tb();
     reg clk;
     reg rst;
-    wire [3:0]up_q;
+    wire [3:0]q;
 
-    async_up_counter inst(.clk(clk),.rst(rst),.q(up_q));
+    async_up_counter inst(clk,rst,q);
 
     initial begin
         clk = 0;
         forever #5 clk = ~clk;
     end
    initial begin
-     $monitor("%0t|clk=%b|rst=%b|up=%b",$time,clk,rst,up_q);
+     $monitor("%0t|clk=%b|rst=%b|q=%b",$time,clk,rst,q);
      $dumpfile("async_up.vcd");
      $dumpvars(0,async_up_counter_tb);
     end
