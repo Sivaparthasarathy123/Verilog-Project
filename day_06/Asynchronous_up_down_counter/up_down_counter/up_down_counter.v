24189b02f5dd3a97d18e6224d53c65cfb4d2cfc6
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-24 01:20:29 +0530

    Update up_down_counter.v

--- day_06/Asynchronous_up_down_counter/up_down_counter/up_down_counter.v
+++ day_06/Asynchronous_up_down_counter/up_down_counter/up_down_counter.v
@@ -4,37 +4,42 @@ module up_down_counter(
   input up_down,
   output reg [3:0]count);
 
-  reg [3:0]temp;
+  reg [3:0]Q;
+  reg [3:0]T;
+
+  always@(*)begin
+    T = 4'b0000;
+  end
   //Flip Flop 0
   always@(posedge clk)begin
     if(rst)
-      temp[0] <= 0;
+      Q[0] <= 0;
     else
-      temp[0] <= ~temp[0];
+      Q[0] <= ~Q[0];
   end
   //Flip Flop 1
-  always@(posedge (up_down ? temp[0] :~temp[0]))begin
+  always@(posedge (up_down ? Q[0] :~Q[0]))begin
     if(rst)
-      temp[1] <= 0;
+      Q[1] <= 0;
     else
-      temp[1] <= ~temp[1];
+      Q[1] <= ~Q[1];
   end
   //Flip Flop 2
-  always@(posedge (up_down ? temp[1] : ~temp[1]))begin
+  always@(posedge (up_down ? Q[1] : ~Q[1]))begin
     if(rst)
-      temp[2] <= 0;
+      Q[2] <= 0;
    else
-     temp[2] <= ~temp[2];
+     Q[2] <= ~Q[2];
   end
   //Flip Flop 3
-  always@(posedge (up_down ? temp[2] : ~temp[2]))begin
+  always@(posedge (up_down ? Q[2] : ~Q[2]))begin
   if(rst)
-    temp[3] <= 0;
+    Q[3] <= 0;
   else
-    temp[3] <= ~temp[3];
+    Q[3] <= ~Q[3];
 end
 
 always@(*)begin 
-  count = temp;
+  count = Q;
 end
 endmodule
