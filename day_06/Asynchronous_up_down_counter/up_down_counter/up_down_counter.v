1b072086f504ae92ae5dc8b989ee3fc1ff1f0f5a
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-24 01:05:52 +0530

    Update up_down_counter.v

--- day_06/Asynchronous_up_down_counter/up_down_counter/up_down_counter.v
+++ day_06/Asynchronous_up_down_counter/up_down_counter/up_down_counter.v
@@ -13,21 +13,21 @@ module up_down_counter(
       temp[0] <= ~temp[0];
   end
   //Flip Flop 1
-  always@(posedge (up_down ? temp[0] :~temp[0])begin
+  always@(posedge (up_down ? temp[0] :~temp[0]))begin
     if(rst)
       temp[1] <= 0;
     else
       temp[1] <= ~temp[1];
   end
   //Flip Flop 2
- always@(posedge (up_down ? temp[1] : ~temp[1])begin
+  always@(posedge (up_down ? temp[1] : ~temp[1]))begin
     if(rst)
       temp[2] <= 0;
    else
      temp[2] <= ~temp[2];
   end
   //Flip Flop 3
-always@(posedge (up_down ? temp[2] : ~temp[2])begin
+  always@(posedge (up_down ? temp[2] : ~temp[2]))begin
   if(rst)
     temp[3] <= 0;
   else
