e2a84b2f930b200eb685e4260b5e974e55dec1f6
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-27 01:31:31 +0530

    SISO.v

--- day_07/SISO/SISO.v
+++ day_07/SISO/SISO.v
@@ -3,22 +3,21 @@ module SISO(
     output reg[3:0]q);
 
     always@(posedge clk)begin
-      if(rst==0)begin
-          sin <= 1'b1;q <= 4'b0000;
-      end
-      else begin
-          sin <= 1'b1;q <= q + 1;
-      end
+      if(rst==0)
+          q <= 4'b0000;
+      else
+          q <= {q[3:0],sin};
     end
+    endmodule
 
 module d_ff(
-    input clk,rst,
-    output q);
+    input clk,rst,d,
+    output reg q);
 
     always@(posedge clk)begin
        if(rst==0)
            q <= 0;
        else
-           q <= q;
-    end
+           q <= d;
+   end
 endmodule
