c1c7f92b654ca46d3515859a32b1afddd2a6bb36
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-27 13:18:34 +0530

    modN_counter.v

--- day_06/modN_counter/modN_counter.v
+++ day_06/modN_counter/modN_counter.v
@@ -1,15 +1,14 @@
 module modN_counter #(
-       parameter N = 16,
-       parameter WIDTH =$clog2(N))
+       parameter N = 4)
        (input clk,rst,
-       output reg [WIDTH -1:0]count);
+        output reg [N-1:0]count);
   
-       wire [WIDTH-1:0]T;
+       wire [N-1:0]T;
        integer j;
 
      genvar i;
      generate 
-         for(i=0;i<WIDTH;i=i+1)begin:Toggle_logic
+            for(i=0;i<16;i=i+1)begin:Toggle_logic
            if(i==0)
                assign T[i]=1'b1;
            else
@@ -19,11 +18,11 @@ module modN_counter #(
 
      always@(posedge clk)begin
         if(rst)
-           count <= {WIDTH{1'b0}};
+               count <= {N{1'b0}};
         else if (count==N-1)
-           count <= {WIDTH{1'b0}};                     
+               count <= {N{1'b0}};                     
         else begin
-            for(j=0;j<WIDTH;j++)begin
+               for(j=0;j<16;j++)begin
                 if (T[j])
                     count[j] <= ~count[j];
         end
