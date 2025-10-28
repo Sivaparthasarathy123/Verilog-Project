624a7c0aeb17fd806a89d9ee8c934694e7008b66
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-10-28 09:52:11 +0530

    Universal_Shift_Register.v

--- day_07/Universal_Shift_Register/Universal_Shift_Register.v
+++ day_07/Universal_Shift_Register/Universal_Shift_Register.v
@@ -5,29 +5,18 @@ module Universal_shift_register #(
       input [N-1:0]pin,
       input RSin,LSin,
       output reg [N-1:0]q);
-
-      /*reg [N-1:0]d;
       
-
-      always@(posedge clk)begin
-          if(rst)begin
-             d <= {N{1'b0}};
-             q <= 1'b0;
-          end
-          else if(load==3)begin
-              d <= pin[N-1:0];
-              q <= d;
-          end
-          else if(load==2)begin
-              d <= {d[N-2:0],RSin};
-           end
-          else if(load==1)begin
-              q <= d >> 1;
-              q <= d << 1;
-          end
-          else if(load==0)begin
-              q <= d;
-          end
+      /*always@(posedge clk)begin
+          if(rst)
+             q <= {N{1'b0}};
+          else if(load==2'b11)
+              q <= pin[N-1:0];
+          else if(load==2'b10)
+              q <= {q[N-2:0],LSin};
+          else if(load==2'b01)
+              q <={RSin,q[N-1:1]};
+          else if(load==2'b00)
+              q <= q;
           end*/
 	  always@(posedge clk)begin
         if (rst)
